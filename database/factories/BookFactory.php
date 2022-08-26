72f52b5356e4d85492aef3a48c2544bb83bae207
Djurdjina Lozo <djurdjinalozo@hotmail.com>
2022-08-26 23:06:37 +0200

    zavrseni factory

--- database/factories/BookFactory.php
+++ database/factories/BookFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Author;
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +20,11 @@ class BookFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->sentance(5),
+            'description' => $this->faker->paragraph(),
+            'category_id' => Category::factory(),
+            'author_id' => Author::factory(),
+            'user_id' => User::factory(),
         ];
     }
 }
